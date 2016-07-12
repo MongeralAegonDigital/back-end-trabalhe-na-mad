123452f49e0cf5fa79dfff9ffc9982317467f73c
Mateus Marques <marques.m05@gmail.com>
2016-07-12 19:34:49 -0300

    finalizando busca por cep

--- public/controllers/ClientController.js
+++ public/controllers/ClientController.js
@@ -4,10 +4,41 @@ angular.module('Mongeral')
     $scope.client = {};
     
     $scope.cepSearch = function(){
-        console.log($scope.client);
+        $scope.cepIncorreto = false;
 
-        CepSearch.execute($scope.client.cep).success(function(data, status){
+        if($scope.client.endereco === undefined){
+            return;
+        }
+
+        CepSearch.execute($scope.client.endereco.cep).success(function(data, status){
             console.log(status);
+            if(status === 200){
+                addInfoCep(data);
+            }
+        }).error(function () {
+            $scope.cepIncorreto = true;
+            clearInfoCep();
         });
+    };
+
+    $scope.submit = function(){
+        ClientService.execute($scope.client).success(function(data, status){
+
+        });
+    };
+
+    var addInfoCep = function(data){
+        $scope.client.endereco.logradouro = data.logradouro;
+        $scope.client.endereco.bairro = data.bairro;
+        $scope.client.endereco.cidade = data.cidade;
+        $scope.client.endereco.uf = data.estado;
+
+        if(data.complemento!==undefined) $scope.client.endereco.complemento = data.complemento;
+        $scope.cepValido = true;
+    };
+    
+    var clearInfoCep = function(){
+        $scope.cepValido = false;
+        $scope.client.endereco = {};
     };
 });
--- public/services/CepSearch.js
+++ public/services/CepSearch.js
@@ -6,20 +6,16 @@ angular.module('Mongeral')
 
         var cepZanitized = _cepValidation(cep);
 
-        if(!cepZanitized){
-            return;
-        }
-
         var url = hostCep + cepZanitized;
 
         return $http.get(url);
     };
 
     var _cepValidation = function(cep) {
-        if (/\d{5}\-\d{3}/.test(cep))
+        if (cep !== undefined && /\d{5}\-\d{3}/.test(cep))
             return cep.replace(/\.|\-/g, '');
         else
-            return false;
+            return "";
     };
     
     return {
