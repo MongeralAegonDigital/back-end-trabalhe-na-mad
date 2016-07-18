b396c44a3f83503b3157b86117231661c2bf3d7e
felyphe <felypheoliveira@hotmail.com>
2016-07-18 01:42:57 -0300

    Add files via upload

---
+++ front/bower_components/jquery-toggles/js/wrap.js
@@ -0,0 +1,26 @@
+/**
+@license jQuery Toggles v3.1.5
+Copyright 2012 - 2015 Simon Tabor - MIT License
+https://github.com/simontabor/jquery-toggles / http://simontabor.com/labs/toggles
+*/
+
+(function(root) {
+
+  var factory = function($) {
+
+    <<Toggles>>
+
+    $.fn['toggles'] = function(opts) {
+      return this.each(function() {
+        new Toggles($(this), opts);
+      });
+    };
+  };
+
+  if (typeof define === 'function' && define['amd']) {
+    define(['jquery'], factory);
+  } else {
+    factory(root['jQuery'] || root['Zepto'] || root['ender'] || root['$'] || $);
+  }
+
+})(this);
