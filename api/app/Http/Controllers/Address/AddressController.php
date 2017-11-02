f2d0ced4b52255128f71dc2c811908080eda7776
broboredo <roboredo.bruno@gmail.com>
2017-11-02 16:51:39 -0200

    Created bundle Address

---
+++ api/app/Http/Controllers/Address/AddressController.php
@@ -0,0 +1,85 @@
+<?php
+namespace App\Http\Controllers\Address;
+
+use App\Models\Address\Address;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Address\Address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Address $address)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Address\Address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Address $address)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Address\Address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Address $address)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Address\Address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Address $address)
+    {
+        //
+    }
+}
---
+++ api/app/Models/Address/Address.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Models\Address;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    //
+}
