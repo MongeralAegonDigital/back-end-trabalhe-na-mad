ca0e8d817c4a4ee0473e164098bc65a7b8459d7e
broboredo <roboredo.bruno@gmail.com>
2017-11-02 17:01:01 -0200

    Change model to bidirectional

--- api/app/Models/Address/Address.php
+++ api/app/Models/Address/Address.php
@@ -1,9 +1,13 @@
 <?php
 namespace App\Models\Address;
 
+use App\Models\User\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'cpf', 'user_cpf');
+    }
 }
