2086797390b90368283d0527cbcb17fa4b8c369b
broboredo <roboredo.bruno@gmail.com>
2017-11-02 16:52:29 -0200

    Created bundle User

---
+++ api/app/Http/Controllers/User/UserController.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Http\Controllers\User;
+
+use App\Models\Address\Address;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Address\Address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Address $address)
+    {
+        //
+    }
+}
--- api/database/factories/UserFactory.php
+++ api/database/factories/UserFactory.php
@@ -13,7 +13,7 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Models\User\User::class, function (Faker $faker) {
     static $password;
 
     return [
@@ -21,5 +21,8 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'cpf' => $faker->randomNumber(),
+        'phone' => $faker->randomNumber(),
+        'date_birth' => $faker->dateTimeAD
     ];
 });
