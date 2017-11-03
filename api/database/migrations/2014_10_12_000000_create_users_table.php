2d1732b6e3deb0374469a815b66444c0b3e9ee69
broboredo <roboredo.bruno@gmail.com>
2017-11-02 23:03:01 -0200

    Update user migration (remove remember_token field)

--- api/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,6 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->string('phone');
             $table->date('date_birth');
-            $table->rememberToken();
             $table->timestamps();            
         });
     }
