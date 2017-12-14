e029bf0679b4701a402a7c51ccc3766c9675d566
eltonc.dev <eltonc.dev@gmail.com>
2017-12-13 22:36:03 -0200

    migrations

---
+++ api/database/migrations/2017_12_13_235520_create_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamp('fabrication_date');
+            $table->float('size',8,2);
+            $table->float('lenght',8,2);
+            $table->float('weight',8,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+    }
+}
