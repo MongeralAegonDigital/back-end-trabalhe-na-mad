80e68e127667da7db928e75fc09116daf7b0c180
CarlosMoreira <kmoreira620@gmail.com>
2018-07-15 23:14:59 -0300

    Register the assign of concrete class for Fake factory with the pt-BR parameter

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(\Faker\Generator::class, function () {
+            return \Faker\Factory::create('pt_BR');
+        });
     }
 }
