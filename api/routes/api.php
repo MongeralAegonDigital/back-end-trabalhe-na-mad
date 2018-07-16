f523f0bac39cd27aedd02a7f634002a545f7c210
CarlosMoreira <kmoreira620@gmail.com>
2018-07-16 00:12:07 -0300

    Create Route group for the prefix 'v1/desafio' and the routes for Client and Email

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +11,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('api')->prefix('v1/desafio')->group(function () {
+    
+    Route::apiResource('clients','ClientController');
+    Route::post('/mails/send', 'EmailController@send');
+    
 });
